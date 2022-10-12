c2df89f85e32601ade1eede75263bc76c31b7d06
Ishiyuka <shijing05youjia@gmail.com>
2022-10-12 14:20:15 +0900

    Generated controller messages and add View

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,62 @@
+class MessagesController < ApplicationController
+    before_action :set_message, only: [:show, :edit, :update,:destroy]
+    
+    def index
+      @messages = Message.all
+    end
+  
+    def new
+      @message = Message.new
+    end
+  
+    def create
+      @message = Message.new(message_params)
+      if params[:back]
+        render :new
+      else
+        if @message.save
+        redirect_to messages_path, notice: "投稿しました！"
+        else
+          render :new
+        end
+      end
+    end
+  
+    def show
+      # @message = Message.find(params[:id])
+    end
+  
+    def edit
+      # @message = Message.find(params[:id])
+    end
+  
+    def update
+      @message = Message.find(params[:id])
+      if @message.update(post_params)
+        redirect_to messages_path, notice: "投稿を編集しました！"
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @post.destroy
+      redirect_to messages_path, notice:"投稿を削除しました！"
+    end
+  
+    def confirm
+      @message = Message.new(message_params)
+      render :new if @message.invalid?
+    end
+  
+      private
+  
+      def message_params
+        params.require(:message).permit(:content)
+      end
+  
+      def set_message
+        @message = Message.find(params[:id])
+      end
+  end
+  
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     before_validation { email.downcase! }
     has_secure_password
-    validates :password, length: { minimum: 10 }                  
+    validates :password, length: { minimum: 10 }  
+    has_many :messages                
   end
