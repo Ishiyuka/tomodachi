ed041294638f372e43b1b16c6b0ca948b4935bde
Ishiyuka <shijing05youjia@gmail.com>
2022-10-13 11:44:19 +0900

    modify Migration file

--- db/migrate/20221012021856_create_messages.rb
+++ db/migrate/20221012021856_create_messages.rb
@@ -1,7 +1,6 @@
 class CreateMessages < ActiveRecord::Migration[6.1]
   def change
     create_table :messages do |t|
-      t.string :title
       t.text :content
       t.references :user, null: false, foreign_key: true
 
